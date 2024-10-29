ff68d796ac5cab49389214080178df743f6e4887
manas-singha <manassingha1346@gmail.com>
2024-10-29 10:53:06 +0530

    revised code

--- q1.py
+++ q1.py
@@ -2,15 +2,12 @@
 num1,num2=0,1
 for i in range(10):
     print(num1)
-    next=num1+num2
-    num1=num2
-    num2=next
+    num1,num2=num2,num1+num2
 #Reverse a given integer number
-number=int(input("enter a number:"))  
+number=int(input("Enter a number:"))
 rev=0
 while number>0:
-    last=number%10
-    rev=rev*10 +last
+    rev=rev*10+number%10
     number=number//10
 print(rev)    
 
