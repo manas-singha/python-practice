cf3928f2510a9423bb2bfbad013331cb31488137
manas-singha <manassingha1346@gmail.com>
2024-11-01 10:47:38 +0530

    bubble_sortoptimize code

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,7 +1,7 @@
 def bubble_sort(arr):
     n=len(arr)
     for passes in range(0,n):
-       for j in range(0,n-1):
+       for j in range(0,n-1-passes):
          if (arr[j]>arr[j+1]):
             arr[j],arr[j+1]=arr[j+1],arr[j]
     return arr        
