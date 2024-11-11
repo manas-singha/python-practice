18896d7e111b28e812961715d3e1778e00a5c13b
manas-singha <manassingha1346@gmail.com>
2024-11-11 23:37:11 +0530

    Nto1 added also

--- 1toN.py
+++ 1toN.py
@@ -4,3 +4,12 @@ def print1toN(n):
     print1toN(n-1)
     print(n)
 print1toN(5)    
+
+
+#print Nto 1
+def print1toN(n):
+    if (n<=0):
+        return
+    print(n)
+    print1toN(n-1)
+print1toN(5)
