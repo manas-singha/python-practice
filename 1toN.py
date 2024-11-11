7221b1e2519f79a0f4074bfd0c1b2bb387e53ecd
manas-singha <manassingha1346@gmail.com>
2024-11-11 23:38:31 +0530

    comment

--- 1toN.py
+++ 1toN.py
@@ -1,7 +1,7 @@
 def print1toN(n):
     if (n<=0):
         return
-    print1toN(n-1)
+    print1toN(n-1)   #use head recursion here
     print(n)
 print1toN(5)    
 
@@ -11,5 +11,5 @@ def print1toN(n):
     if (n<=0):
         return
     print(n)
-    print1toN(n-1)
+    print1toN(n-1)   #use tail recursion here
 print1toN(5)
