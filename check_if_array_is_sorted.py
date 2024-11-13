2c34d295f64c0bc7766a302f470e75251a84b85a
manas-singha <manassingha1346@gmail.com>
2024-11-13 09:53:40 +0530

    List

---
+++ check_if_array_is_sorted.py
@@ -0,0 +1,9 @@
+def checkSorted(l1):  #using Recursion
+    if(len(l1)==0 or len(l1)==1):
+        return True
+    ans=checkSorted(l1[1:])
+    if (l1[0] < l1[1]):
+        return ans
+    else:
+        return False
+print(checkSorted([2,5,7,9,10]))    
